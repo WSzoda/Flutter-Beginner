8e072784e4efc12e75fbb344748ab5bab20bcdc0
Wojciech Szoda <wojciechszoda@icloud.com>
2023-02-15 10:43:33 +0100

    CHapter 5

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -12,7 +12,141 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: const Text('Home'),
       ),
-      body: Container(),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: const [
+                RowWidget(),
+                Padding(
+                  padding: EdgeInsets.all(16),
+                ),
+                RowAndColumnWidget(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RowWidget extends StatelessWidget {
+  const RowWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          color: Colors.yellow,
+          height: 40,
+          width: 40,
+        ),
+        const Padding(
+          padding: EdgeInsets.all(16),
+        ),
+        Expanded(
+          child: Container(
+            color: Colors.amber,
+            height: 40,
+            width: 40,
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.all(16),
+        ),
+        Container(
+          color: Colors.brown,
+          height: 40,
+          width: 40,
+        ),
+      ],
+    );
+  }
+}
+
+class RowAndColumnWidget extends StatelessWidget {
+  const RowAndColumnWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Container(
+              color: Colors.yellow,
+              height: 60,
+              width: 60,
+            ),
+            const Padding(
+              padding: EdgeInsets.all(16),
+            ),
+            Container(
+              color: Colors.amber,
+              height: 40,
+              width: 40,
+            ),
+            const Padding(
+              padding: EdgeInsets.all(16),
+            ),
+            Container(
+              color: Colors.brown,
+              height: 20,
+              width: 20,
+            ),
+            const Divider(),
+            const RowAndStackWidget(),
+            const Divider(),
+            const Text('End of the line')
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class RowAndStackWidget extends StatelessWidget {
+  const RowAndStackWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        CircleAvatar(
+          backgroundColor: Colors.lightGreen,
+          radius: 100,
+          child: Stack(
+            children: [
+              Container(
+                color: Colors.yellow,
+                height: 100,
+                width: 100,
+              ),
+              Container(
+                color: Colors.amber,
+                height: 60,
+                width: 60,
+              ),
+              Container(
+                color: Colors.brown,
+                height: 40,
+                width: 40,
+              ),
+            ],
+          ),
+        )
+      ],
     );
   }
 }
