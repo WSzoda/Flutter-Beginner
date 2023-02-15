56ffd60d869647b9c99625dda3ad5cb18f3f1c4b
Wojciech Szoda <wojciechszoda@icloud.com>
2023-02-15 09:17:21 +0100

    Chapter 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,18 @@
 import "package:flutter/material.dart";
 import 'package:pogoda/pages/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Starter Template',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Home(),
+    );
+  }
+}
